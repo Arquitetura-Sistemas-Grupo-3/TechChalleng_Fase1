88866637288e829bda70f473eb1696ae81f01cdb
Marcos Vinicius <viniciushp00@gmail.com>
2026-07-16 20:23:57 -0300

    Adicionando Seed de niveis de acesso na Migration

--- Infra/Repository/Configurations/NivelAcessoConfiguration.cs
+++ Infra/Repository/Configurations/NivelAcessoConfiguration.cs
@@ -17,6 +17,11 @@ namespace Infra.Repository.Configurations
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).HasColumnType("INT").UseIdentityColumn();
             builder.Property(x => x.Nome).IsRequired().HasMaxLength(100);
+
+            builder.HasData(
+                new NivelAcesso {Id= 1 ,Nome = "Admin" },
+                new NivelAcesso {Id = 2,Nome = "Usuário" }
+                );
         }
     }
 }
