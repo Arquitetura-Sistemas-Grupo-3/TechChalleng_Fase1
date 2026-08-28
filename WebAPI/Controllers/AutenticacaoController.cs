3d6fe8cbed21d315e0a04f042c27f7b3e3b9dcde
Marcos Vinicius <viniciushp00@gmail.com>
2026-08-28 19:33:13 -0300

    Ajuste no nome de variáveis para português

--- WebAPI/Controllers/AutenticacaoController.cs
+++ WebAPI/Controllers/AutenticacaoController.cs
@@ -24,15 +24,15 @@ namespace WebAPI.Controllers
         /// Autentica um usuário e retorna o token JWT de acesso.
         /// </summary>
         /// <param name="email">E-mail do usuário.</param>
-        /// <param name="password">Senha do usuário.</param>
+        /// <param name="senha">Senha do usuário.</param>
         /// <returns>Nome do usuário e token JWT.</returns>
         /// <response code="401">Credenciais inválidas.</response>
         [HttpGet]
         [ProducesResponseType(typeof(ServiceResponse<AutenticarResposta>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ServiceResponse), StatusCodes.Status401Unauthorized)]
-        public async Task<IActionResult> Autenticar(string email, string password)
+        public async Task<IActionResult> Autenticar(string email, string senha)
         {
-            var response = await _authService.Autenticar(email, password);
+            var response = await _authService.Autenticar(email, senha);
 
             if (!response.Success) return Unauthorized(response);
 
--- WebAPI/Interface/IAuthService.cs
+++ WebAPI/Interface/IAuthService.cs
@@ -5,6 +5,6 @@ namespace WebAPI.Interface
 {
     public interface IAuthService
     {
-        Task<ServiceResponse<AutenticarResposta>> Autenticar(string email, string password);
+        Task<ServiceResponse<AutenticarResposta>> Autenticar(string email, string senha);
     }
 }
